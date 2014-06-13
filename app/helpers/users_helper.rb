1a45bc4a5ac4229a2463bb74f3898e06337ce68d
Vasiliy <neo03.03.67@gmail.com>
2014-06-13 11:02:24 +0400

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	#Returns the gravatar for the given user(http://gravatar.com)
+	def gravatar_for(user, options = {size: 60})
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
